b65c92952f91d5c309fb4acb124131de900490a3
JRproyects <168209924+JRproyects@users.noreply.github.com>
2025-10-21 14:11:02 -0300

    remoto

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// Configuración para un proyecto con React
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/App-Milka/' // <--- el nombre de tu repo
+});
